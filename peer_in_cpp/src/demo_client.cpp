2f1b8e4ec0afa712bf0f8cf4d9fd719c758105ae
xjurack1 <xjurack1@fi.muni.cz>
2022-05-21 15:10:35 +0200

    created default project file, CMakeLists.txt adjustments

---
+++ peer_in_cpp/src/demo_client.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+
+int main() { std::cout << "This will be a great client ... one day\n"; }
---
+++ peer_in_cpp/src/demo_server.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+
+int main() { std::cout << "This will be a great server ... one day\n"; }
