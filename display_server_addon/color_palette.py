29dd93cea61cbbbb1f1f13f8d4906f4299ce7024
Vladimír Ulman <ulman@fi.muni.cz>
2022-11-02 12:35:43 +0100

    ADD,CHG: color palette seems to be finished

--- display_server_addon/color_palette.py
+++ display_server_addon/color_palette.py
@@ -9,8 +9,6 @@ class ColorPalette:
         else:
             self.init_using_rgb_quantization(R_elems, G_elems, B_elems)
 
-        self.blender_node = None
-
 
     def init_from_list(self, list_of_namergb_quartets):
         # a quartet is: name_string, r_elem_range_0-1, g_elem, b_elem
@@ -84,6 +82,22 @@ class ColorPalette:
         return self.get_index_for_rgb_0_1(r/255.0, g/255.0, b/255.0)
 
 
+    def get_index_for_XRGB(self, XRGB:int):
+        r = (XRGB // 65536) % 256
+        g =  (XRGB // 256)  % 256
+        b =      XRGB       % 256
+        return self.get_index_for_rgb_0_1(r/255.0, g/255.0, b/255.0)
+
+
+    def get_or_create_new_material(self, mat_name, r,g,b, alpha=1):
+        mat = bpy.data.materials.get(mat_name)
+        if mat is None:
+            mat = bpy.data.materials.new(mat_name)
+            mat.diffuse_color = [r,g,b, alpha]
+            mat.roughness = 10
+        return mat
+
+
     def create_blender_node_into_current_collection(self, new_node_name: str, is_visible: bool = False):
         # a small, 4-point grid
         bpy.ops.mesh.primitive_grid_add(x_subdivisions=1, y_subdivisions=1)
@@ -97,13 +111,34 @@ class ColorPalette:
         ref_obj.data.vertices.add(1)
 
         for [n,r,g,b] in self.palette.values():
-            mat = bpy.data.materials.new(n)
-            mat.diffuse_color = [r,g,b,1]
-            mat.roughness = 10
-
-            bpy.ops.object.material_slot_add()
-            ref_obj.active_material = mat
+            ref_obj.data.materials.append( self.get_or_create_new_material(n, r,g,b) )
 
         ref_obj.active_material_index = 0
         ref_obj.hide_viewport = not is_visible
         return ref_obj
+
+
+    def create_blender_reference_colored_nodes_into_new_collection(self, ref_shape_blender_obj, new_collection_name):
+        # clones the reference shape into many copies and attaches one color from our
+        # palette to each of these copies; essentially clones and colors the ref shape
+        new_col = bpy.data.collections.new(new_collection_name)
+        bpy.data.collections['Reference shapes'].children.link(new_col)
+
+        idx = 0
+        for [n,r,g,b] in self.palette.values():
+            # create an independent clone of the reference shape
+            o = ref_shape_blender_obj.copy()
+            o.data = ref_shape_blender_obj.data.copy() # to be able to have own materials
+            o.hide_viewport = True
+
+            # give it a sort-order-preserving name
+            o.name = f"{idx}: {n}"
+            idx += 1
+
+            # assign the material
+            o.active_material = self.get_or_create_new_material(n, r,g,b)
+
+            # make sure it ends up in the new collection
+            new_col.objects.link(o)
+
+        return new_col
