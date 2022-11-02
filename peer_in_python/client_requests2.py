472e934865382218f674f960c558f7c444a50ecf
Vladimír Ulman <ulman@fi.muni.cz>
2022-11-02 19:22:19 +0100

    FIX,CHG: adapted client_requests2 demo code to show the new protocol

--- peer_in_python/client_requests2.py
+++ peer_in_python/client_requests2.py
@@ -35,7 +35,7 @@ def main() -> None:
             bucket.clientID.clientName = clientName
             bucket.bucketID = 121
             bucket.label = f"many spheres, row {j+1}"
-            bucket.time = 9
+            bucket.time = 15
             for i in range(noOfSpheres):
                 x = i // 100
                 y = i % 100
@@ -44,7 +44,7 @@ def main() -> None:
                 sphParams.centre.y = y
                 sphParams.centre.z = j
                 sphParams.radius = 0.3
-                sphParams.colorXRGB = 80+ 80*(i % 3)
+                sphParams.colorIdx = i % 65
                 bucket.spheres.append(sphParams)
             buckets.append(bucket)
         comm.addSpheres(iter(buckets))
@@ -53,7 +53,7 @@ def main() -> None:
         bucket.clientID.clientName = clientName
         bucket.bucketID = 128
         bucket.label = "testing lines"
-        bucket.time = 6
+        bucket.time = 18
         #
         line = buckets_with_graphics_pb2.LineParameters()
         line.startPos.x = 10
@@ -61,14 +61,15 @@ def main() -> None:
         line.startPos.z = 16
         line.endPos.x = 15
         line.endPos.y = 10
-        line.endPos.z = 16
+        line.endPos.z = 17
         line.radius = 2
-        line.colorXRGB = 0x00FFFFFE
+        line.colorIdx = 0
+        bucket.lines.append(line)
         #
         line = buckets_with_graphics_pb2.LineParameters()
         line.startPos.x = 10
         line.startPos.y = 10
-        line.startPos.z = 16
+        line.startPos.z = 15
         line.endPos.x = 15
         line.endPos.y = 10
         line.endPos.z = 16
@@ -79,12 +80,12 @@ def main() -> None:
         line = buckets_with_graphics_pb2.LineParameters()
         line.startPos.x = 10
         line.startPos.y = 15
-        line.startPos.z = 16
+        line.startPos.z = 15
         line.endPos.x = 15
         line.endPos.y = 15
         line.endPos.z = 16
         line.radius = 2
-        line.colorXRGB = 0x00FF0000
+        line.colorIdx = 1
         bucket.lines.append(line)
         #
         line = buckets_with_graphics_pb2.LineParameters()
@@ -93,14 +94,14 @@ def main() -> None:
         line.startPos.z = 16
         line.endPos.x = 15
         line.endPos.y = 15
-        line.endPos.z = 16
+        line.endPos.z = 17
         line.radius = 2
         line.colorXRGB = 0x00FF0000
         bucket.lines.append(line)
         #
         stream_of_lines = list()
         stream_of_lines.append(bucket)
-        #comm.addVectors(iter(stream_of_lines))
+        comm.addVectors(iter(stream_of_lines))
 
     except RpcError as e:
         print("Some connection error, details follow:")
